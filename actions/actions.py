74e48b12909a522cdec5c5526291b9cd169d8572
Nele Albers <nele.96@web.de>
2023-11-17 13:00:23 +0100

    choose from 0 1 for human support

--- actions/actions.py
+++ actions/actions.py
@@ -609,7 +609,7 @@ class ActionChooseHumanSupport(Action):
         # do not get an RL sample starting in session 5. So human support there
         # would be wasted.
         if session_num <= 4:
-            human_support_after_session = np.random.choice([False, True], size = 1, 
+            human_support_after_session = np.random.choice([0, 1], size = 1, 
                                                            p = [1-PROB_HUMAN_SUPPORT, PROB_HUMAN_SUPPORT])
         else:
             human_support_after_session = False
